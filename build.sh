b353fe23e51bde32893724e27907804e0727094a
Aderibigbe Ganiu Adeleke <58689508+aderibigbeganiu@users.noreply.github.com>
2024-03-20 12:52:30 +0100

    static

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
 # python manage.py createsuperuser --noinput
