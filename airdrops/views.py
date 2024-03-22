810a0f4b5e01f76f2da37d0dd851f4f9039a929e
Aderibigbe Ganiu Adeleke <58689508+aderibigbeganiu@users.noreply.github.com>
2024-03-22 16:21:16 +0100

    filter

--- airdrops/views.py
+++ airdrops/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters.rest_framework import DjangoFilterBackend
 from airdrops.serializers import AirdropSerializer
 from .models import Airdrop
 
@@ -7,4 +7,6 @@ class AirdropViewset(ModelViewSet):
     queryset = Airdrop.objects.all()
     serializer_class = AirdropSerializer
     lookup_field = "slug"
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["categories__name"]
     
